730486922e97951ba3ed98f1d7cbf11852c9dea6
ti-89 <96961887+ti-89@users.noreply.github.com>
2026-02-10 19:50:49 -0600

    Update music URL to use relative path

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: source src="Soulja Boy - Kiss Me Thru the Phone.mp3" type="audio/mpeg"
+        musicUrl: "music/kiss-me-thru-the-phone.mp3",
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.75                        // Volume level (0.0 to 1.0)
