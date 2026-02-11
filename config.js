143929d12bb0b1f74460b104b7777c093e84130f
ti-89 <96961887+ti-89@users.noreply.github.com>
2026-02-10 19:41:46 -0600

    Fix music source URL formatting in config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        source src="Soulja Boy - Kiss Me Thru the Phone.mp3" type="audio/mpeg"
+        musicUrl: source src="Soulja Boy - Kiss Me Thru the Phone.mp3" type="audio/mpeg"
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.75                        // Volume level (0.0 to 1.0)
